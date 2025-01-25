38b957dc5cf745ec786f75dd0cf2bc99a56dea8e
ShenSingh <gayashan9090@gmail.com>
2025-01-25 21:51:14 +0530

    create dto

---
+++ src/main/java/com/zenveus/ecommerce/dto/FollowerDTO.java
@@ -0,0 +1,14 @@
+package com.zenveus.ecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FollowerDTO {
+    private String username;
+    private String email;
+    private String password;
+}
